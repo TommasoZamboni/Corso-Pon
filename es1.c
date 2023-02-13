c5e5e9cfa6c04cbe0fa004053d272e588518abb6
Tommaso <tommaso.zamboni@iisviolamarchesini.edu.it>
2023-02-13 15:30:24 +0100

    es1

---
+++ es1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/fcntl.h>
+#include <ctype.h>
+#include <string.h>
+int main()
+{
+    printf("Hello world");
+}
